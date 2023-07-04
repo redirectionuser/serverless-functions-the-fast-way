00aafa608163ebf3a0b631c097fbc27fd7ef4f13
Redirection User <redirection.user@outlook.com>
2023-07-04 17:45:45 +0200

    added vscodium-latest cloud function

--- functions/hello.ts
+++ functions/hello.ts
@@ -3,6 +3,9 @@ import type { Handler, HandlerEvent, HandlerContext } from "@netlify/functions";
 const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
   return {
     statusCode: 200,
+    headers: {
+        'Content-Type': 'application/json',
+    },
     body: JSON.stringify({ message: "Hello World" }),
   };
 };
